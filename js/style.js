887823c0b8bdac1ffa6f4c87dfc0461d0bd1a08b
vinsekani <vinsekani@gmail.com>
2024-11-11 16:26:56 +0300

    i have fetched jokes and added add an event listener on javascript

--- js/style.js
+++ js/style.js
@@ -1,2 +1,20 @@
 
+const jokesButton = document.querySelector('.button')
+
+
+
+
+const getJokes = async() =>{
+    const response = await fetch('https://api.chucknorris.io/jokes/random')
+
+    const randomJokes = await response.json()
+   document.querySelector('p').innerText = randomJokes.value
+    dogImage.src = data.message
+}
+
+getJokes()
+
+jokesButton.addEventListener('click', function(e){
+    getJokes()
+})
 
