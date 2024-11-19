f36f28f21e123799e6fd56d8ad31e230fd6d14ee
vinsekani <vinsekani@gmail.com>
2024-11-19 16:56:42 +0300

    printed some values to the console

--- js/style.js
+++ js/style.js
@@ -1,13 +1,13 @@
 
 const jokesButton = document.querySelector('.button')
-
-
-
+console.log(jokesButton)
 
 const getJokes = async() =>{
     const response = await fetch('https://api.chucknorris.io/jokes/random')
+    console.log(response)
 
     const randomJokes = await response.json()
+    console.log(randomJokes)
    document.querySelector('p').innerText = randomJokes.value
 }
 
